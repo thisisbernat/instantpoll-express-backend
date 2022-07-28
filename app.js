d71447da2a4cf6aa643bb43d21c26e620c2d9612
Bernat Escarrà <bernat.escarra@gmail.com>
2022-07-28 22:22:52 +0200

    Update app.js for CORS policy

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require("dotenv/config");
 require("./db");
 const express = require("express");
+const cors = require('cors');
 
-const { isAuthenticated } = require("./middleware/jwt.middleware"); // <== IMPORT
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 
 
 const app = express();
 require("./config")(app);
+
+app.use(cors({
+    origin: '*'
+}));
 
 
 // 👇 MIDDLEWARE MISSING
@@ -16,25 +21,15 @@ app.use("/api", allRoutes);
 const authRouter = require("./routes/auth.routes");
 app.use("/api/auth", authRouter);
 
-const projectRouter = require("./routes/project.routes");
-app.use("/api", isAuthenticated, projectRouter);
-// app.use("/api", projectRouter);
-
-const taskRouter = require("./routes/task.routes");
-app.use("/api",isAuthenticated, taskRouter);
-// app.use("/api", taskRouter);
-
 const pollRouter = require("./routes/poll.routes");
 app.use("/api",isAuthenticated, pollRouter);       
-// app.use("/api", pollRouter);
 
 const questionRouter = require("./routes/question.routes");
 app.use("/api",isAuthenticated, questionRouter);      
-// app.use("/api", questionRouter);
 
 const answerRouter = require("./routes/answer.routes");
 app.use("/api",isAuthenticated, answerRouter);           
-// app.use("/api", answerRouter);
+
 
 
 // app.use((req, res, next) => {
